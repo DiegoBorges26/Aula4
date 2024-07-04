fda2e5a2ba2447d34dac2d48beedbe9efae2f11c
DiegoBorges26 <diego.fort2011@hotmail.com>
2024-07-04 12:16:10 -0300

    Add files via upload

---
+++ Aula4/script.js
@@ -0,0 +1,65 @@
+// PRATICA - ATIVIDADE 1
+//var numero = 1
+//while ( numero <= 10) {
+   // console.log(numero)
+    //numero++
+//}
+//----------------------------------------------
+//PRATICA - ATIVIDADE 2
+ 
+//var somatorio  = 0
+//
+//while (true){
+   // var numero = prompt(`Digite um numero`);
+   // somatorio =  somatorio + parseFloat (numero);
+   // console.log(somatorio);
+   // 
+//
+//if(isNaN(numero)){
+  //  break;
+//}
+//}
+//-----------------------------------------------------
+//PRATICA - ATICVIDADE 3
+
+// let random = parseInt(Math.random()*10)
+// console.log(random)
+
+//     while (true){
+//         var adivinha  = parseInt(prompt(`Digite um numero no navegador`));
+
+//         if( adivinha == random){
+//             break
+//         }
+//     }
+//---------------------------------------------------------
+//PRATICA - PRATIQUE E APRENDA
+
+// var saldo = 0
+// var toggleLayout = true
+// while (toggleLayout){
+//     var layout = prompt(`Selecione a opção que deseja utilizar. \n 1.Ver Saldo. \n 2.Depositar. \n 3.Sacar. \n 4.Sair.`)
+//     switch (layout) {
+//         case "1":
+//             console.log(`Você possui um saldo de ${saldo}`)
+//             break;
+//         case "2": 
+//             var deposito = prompt(`Digite o valor que quer depositar`) 
+//             saldo = saldo + parseFloat(deposito)
+//             console.log(`Seu saldo atual é ${saldo}`) 
+//             break;
+        
+//         case "3":
+//             var saque = prompt(`Digite o valor que deseja retira`)
+//             saldo = saldo - parseFloat(saque)
+//             console.log(`Seu novo saldo é ${Saldo}`)
+//         case "4":
+//              console.log(`Você saiu do menu`)
+//              toggleLayout = false
+//              break;
+        
+
+//         default:
+//             break;
+//     }
+// }
